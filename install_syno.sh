09e850fe9e26232b9947d08564a1b49a3b71ac89
Charly Poppins <CharlyPoppins@users.noreply.github.com>
2016-01-03 15:28:24 +0100

    Update install_syno.sh

--- install_syno.sh
+++ install_syno.sh
@@ -18,8 +18,8 @@ chmod 777 jeedom.sh
 ./jeedom.sh stop
 sed -i -e "s/3306/3336/g" /etc/mysql/my.cnf
 sed -i -e "s/3306/3336/g" /usr/share/nginx/www/jeedom/core/config/common.config.php
-wget --no-check-certificate https://github.com/sarakha63/Jeedom_Syno/raw/master/Chroot/default
-wget --no-check-certificate https://github.com/sarakha63/Jeedom_Syno/raw/master/Chroot/default_ssl
+wget --no-check-certificate https://raw.githubusercontent.com/CharlyPoppins/JeedomSynologyDSM5.2/master/default
+wget --no-check-certificate https://raw.githubusercontent.com/CharlyPoppins/JeedomSynologyDSM5.2/master/default_ssl
 mv default /etc/nginx/sites-available
 mv default_ssl /etc/nginx/sites-available
 ./jeedom.sh start
