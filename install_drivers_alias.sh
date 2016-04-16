9c4912d3f074dcaa826a4bc6a00899290dc693e8
Charly Poppins <CharlyPoppins@users.noreply.github.com>
2016-04-16 17:22:50 +0200

    Update install_drivers_alias.sh

--- install_drivers_alias.sh
+++ install_drivers_alias.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/cp210x.ko
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/ftdi_sio.ko
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/usbserial.ko
-wget http://github.com/sarakha63/Jeedom_Syno/raw/master/Syno/S99Modules.sh
+wget https://github.com/CharlyPoppins/JeedomSynologyDSM5.2/raw/master/cp210x.ko
+wget https://github.com/CharlyPoppins/JeedomSynologyDSM5.2/raw/master/ftdi_sio.ko
+wget https://github.com/CharlyPoppins/JeedomSynologyDSM5.2/raw/master/usbserial.ko
+wget https://github.com/CharlyPoppins/JeedomSynologyDSM5.2/raw/master/S99Modules.sh
 mv *.ko /lib/modules
 mv S99Modules.sh /usr/syno/etc.defaults/rc.d/
 chmod 777 /usr/syno/etc.defaults/rc.d/S99Modules.sh
